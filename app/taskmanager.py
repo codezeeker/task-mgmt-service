c0ec8d787b698b4dd7ff8f3a5444e256156ff800
ahamed <ahamedsha@outlook.in>
2020-11-02 22:24:23 +0530

    fully functioning code of taskmanager.py

--- app/taskmanager.py
+++ app/taskmanager.py
@@ -1,18 +1,14 @@
-
-cont = "y"
-
-while cont == "y":
-    
-  #step 1 - get input from user
-
-  #step 2 - add it to the list 
-
-  #step 3 - display the list to the console
-  print("Here are the task list for today:")
-
-
-  cont = input("Do you want to continue?[y/n] :")
-
-
+stuffs = []
+while True:
+    print("add your list item:")
+    items = input()
+    stuffs.append(items)
+    print(stuffs)
+    cont = input("Do you want to continue?")
+    if cont == "y":
+        continue
+    elif cont == "n":
+        break
+print("here is your list of items today: ", stuffs)
 print(" -------------- ")
 print("Have a good day")
