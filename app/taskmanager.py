b7dc9c5f6bf13b9196d6906de1b0c44549fbceb6
Ziaur Rahman <ziaur25@gmail.com>
2020-11-02 14:33:27 +0000

    #3 - added a template to work for the #3 issue

---
+++ app/taskmanager.py
@@ -0,0 +1,18 @@
+
+cont = "y";
+
+while cont == "y":
+    
+  #step 1 - get input from user
+
+  #step 2 - add it to the list 
+
+  #step 3 - display the list to the console
+  print("Here are the task list for today:")
+
+
+  cont = input("Do you want to continue?[y/n] :")
+
+
+print(" -------------- ")
+print("Have a good day")
