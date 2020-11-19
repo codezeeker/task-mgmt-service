13a82e93b73b05959793c59aab05bd47f82cb397
ahamed <ahamedsha@outlook.in>
2020-11-19 20:44:31 +0530

    Unable to retrieve

--- app/taskmanager-db.py
+++ app/taskmanager-db.py
@@ -1,7 +1,6 @@
 import sqlite3
 from sqlite3 import Error
 
-
 def create_connection():
     """ create a database connection to a SQLite database """
     conn = None
@@ -12,21 +11,32 @@ def create_connection():
         curs.execute("CREATE TABLE IF NOT EXISTS todo (item TEXT)")
         list_item = input("add the task: ")
         curs.execute("INSERT INTO todo(item) VALUES(?)", [list_item])
-
-
-        #test
-        # curs.execute("SELECT * from todo")
-        # rows = curs.fetchall()
-        # print(rows)
-
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
 
+def retrieve_rows():
+    conn = None
+    try:
+        conn = sqlite3.connect(':memory:')
+        curs = conn.cursor()
+        curs.execute("SELECT * from todo")
+        rows = curs.fetchall()
+        print(rows)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+# Main loop
 cont = "y"
 while cont == "y":
-
     create_connection()
     cont = input("\no you want to continue[y/n]: ")
+
+    if cont == "n":
+        print("Here is your list:")
+        retrieve_rows()
