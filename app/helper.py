12303d1ef929d035ee6837a5d083e36c8fb4b35c
ahamed <ahamedsha@outlook.in>
2020-11-28 11:11:26 +0530

    "update" feature added

--- app/helper.py
+++ app/helper.py
@@ -2,15 +2,20 @@ import sqlite3
 from flask import Flask, jsonify
 from sqlite3 import Error
 
+# status
+PENDING = "pending"
+COMPLETED = "completed"
+
+
 def add_task(item):
     conn = None
     try:
         # conn = sqlite3.connect(':memory:')
         conn = sqlite3.connect('mydatabase.db')
         curs = conn.cursor()
-        curs.execute("INSERT INTO todo(item) VALUES(?)", ["item"])
+        curs.execute("INSERT INTO todo(item, status) VALUES(?,?)", (item, PENDING))
         curs.execute("COMMIT")
-        return {"item": item}
+        return {"item": item, "status": PENDING}
     except Error as e:
         print(e)
     finally:
@@ -24,8 +29,7 @@ def retrieve_rows():
         curs = conn.cursor()
         curs.execute("SELECT * from todo")
         rows = curs.fetchall()
-        print(rows)
-        return jsonify({'tasks': rows})
+        return rows
     except Error as e:
         print(e)
     finally:
@@ -36,10 +40,32 @@ def delete_task(item):
     conn = None
     try:
         conn = sqlite3.connect('mydatabase.db')
-        c = conn.cursor()
-        c.execute('DELETE FROM todo WHERE item=?', (item,))
-        conn.commit()
+        curs = conn.cursor()
+        curs.execute('DELETE FROM todo WHERE item=?', (item,))
+        curs.execute("COMMIT")
         return {'item': item}
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+def update_status(item, status):
+    #check
+    if (status.lower().strip() == 'pending'):
+        status = PENDING
+    elif (status.lower().strip() == 'completed'):
+        status = COMPLETED
+    else:
+        print("Invalid Status: " + status)
+        return None
+
+    try:
+        conn = sqlite3.connect('mydatabase.db')
+        curs = conn.cursor()
+        curs.execute('update items set status=? where item=?', (status, item))
+        curs.execute("COMMIT")
+        return {item: status}
     except Error as e:
         print(e)
     finally:
--- app/rest.py
+++ app/rest.py
@@ -19,7 +19,7 @@ def create_db():
         # conn = sqlite3.connect(':memory:')
         conn = sqlite3.connect('mydatabase.db')
         curs = conn.cursor()
-        curs.execute("CREATE TABLE IF NOT EXISTS todo (item TEXT NOT NULL PRIMARY KEY)")
+        curs.execute("CREATE TABLE IF NOT EXISTS todo (item TEXT NOT NULL, status TEXT NOT NULL, PRIMARY KEY(item))")
         curs.execute("COMMIT")
     except Error as e:
         print(e)
@@ -27,8 +27,9 @@ def create_db():
         if conn:
             conn.close()
 
-# Add item to list
 
+
+# Add item to list
 @app.route('/item/new', methods=['POST'])
 def add_item():
 
@@ -47,14 +48,17 @@ def add_item():
     response = Response(json.dumps(res_data), mimetype='application/json')
     return response
 
+
+
 # Retrieve table
 @app.route("/items/all", methods=['GET'])
 def get_all_items():
+
     # Get items from the helper
-    res_data = helper.retrieve_rows()
+    rows = helper.retrieve_rows()
 
     # Return response
-    response = Response(json.dumps(res_data), mimetype='application/json')
+    response = Response(json.dumps({'tasks': rows}), mimetype='application/json')
     return response
 
 
@@ -62,24 +66,40 @@ def get_all_items():
 # Delete the task
 @app.route('/item/remove', methods=['DELETE'])
 def delete_item():
-    # Get item from the POST body
+
     req_data = request.get_json()
     item = req_data['item']
 
-    # Delete item from the list
     res_data = helper.delete_task(item)
 
-    # Return error if the item could not be deleted
+    # Return error
     if res_data is None:
         response = Response("{'error': 'Error deleting item - '" + item +  "}", status=400 , mimetype='application/json')
         return response
 
     # Return response
     response = Response(json.dumps(res_data), mimetype='application/json')
-
     return response
 
 
+# Update task
+@app.route('/item/update', methods=['PUT'])
+def update_status():
+    
+    req_data = request.get_json()
+    item = req_data['item']
+    status = req_data['status']
+
+    res_data = helper.update_status(item, status)
+
+    # Return error
+    if res_data is None:
+        response = Response("{'error': 'Error updating item - '" + item + ", " + status   +  "}", status=400 , mimetype='application/json')
+        return response
+
+    # Return response
+    response = Response(json.dumps(res_data), mimetype='application/json')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
